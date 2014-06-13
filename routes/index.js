d285725a79f93d41cf29de6f6c0fedf257abbfa2
Christopher Lubaway <asimplechris@gmail.com>
2014-06-13 16:49:47 -0700

    Basic Timer with static date functioning Polls every second and changes coundown.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Boot Hack Timer' });
+  res.render('index', {scripts: ['javascripts/timer.js'], title: 'Boot Hack Timer' });
 });
 
 module.exports = router;
