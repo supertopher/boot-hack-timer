68c2dcb0e220cb8279ee196b2fe75675939429c9
Christopher Lubaway <asimplechris@gmail.com>
2014-06-17 15:27:18 -0700

    Travis API call working data on in JS Need to add DOM manipulation and MVC

--- public/javascripts/travis.js
+++ public/javascripts/travis.js
@@ -1,10 +1,15 @@
+var travis_data, last_commit, last_build;
+
 document.addEventListener('DOMContentLoaded', function(){
 
   $.ajax({
     url: "https://api.travis-ci.org/repos/red-admirals-2014/BootTrack/builds",
     headers: { Accept: "application/vnd.travis-ci.2+json" },
     success: function(data) {
-      console.log(data);
+      // console.log(data);
+      travis_data = data;
+      last_build  = travis_data.builds[0];
+      last_commit = travis_data.commits[0];
      }
   });
 
